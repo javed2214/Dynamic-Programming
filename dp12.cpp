46074746894a319f55134746a77526ab039d897a
Javed Ansari <46120375+javed2214@users.noreply.github.com>
2018-12-26 00:51:34 +0530

    Update dp12.cpp

--- dp12.cpp
+++ dp12.cpp
@@ -1,4 +1,4 @@
-// Minimum Steps To One Problem. Can Perform only Following Operations
+// "Minimum Steps To One" Problem. Can Perform only Following Operations
 // If n is divisible by 2 then we may reduce n to n/2.
 // If n is divisible by 3 then you may reduce n to n/3.
 // Decrement n by 1.
