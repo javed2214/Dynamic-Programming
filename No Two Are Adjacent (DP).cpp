08b622e92867d75acee767cd25eb0af277ba0515
Javed Ansari <46120375+javed2214@users.noreply.github.com>
2019-05-27 16:34:09 +0530

    Update No Two Are Adjacent (DP).cpp

--- No Two Are Adjacent (DP).cpp
+++ No Two Are Adjacent (DP).cpp
@@ -3,6 +3,18 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+/*
+
+int dp[n];
+dp[0]=9;dp[1]=3;
+
+for(int i=2;i<n;i++)
+	dp[i]=max(a[i]+dp[i-2], dp[i-1]);
+
+cout<<dp[n-1];
+
+*/
+
 int findMax(int *a, int n){
 	int incl, excl, x;
 	incl=a[0];
