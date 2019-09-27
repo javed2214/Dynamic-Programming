dce29440801b313cea07747a581afb16cc7c3e14
Javed Ansari <46120375+javed2214@users.noreply.github.com>
2019-09-28 00:33:14 +0530

    Update No Two Are Adjacent (DP).cpp

--- No Two Are Adjacent (DP).cpp
+++ No Two Are Adjacent (DP).cpp
@@ -5,13 +5,20 @@ using namespace std;
 
 /*
 
-int dp[n];
-dp[0]=9;dp[1]=3;
-
-for(int i=2;i<n;i++)
-	dp[i]=max(a[i]+dp[i-2], dp[i-1]);
-
-cout<<dp[n-1];
+	int n = nums.size();
+        
+        if(n == 0) return 0;
+        if(n == 1) return nums[0];
+        if(n == 2) return max(nums[0], nums[1]);
+        
+        int dp[n];
+        
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
+        
+        for(int i=2;i<n;i++) dp[i] = max(nums[i] + dp[i-2], dp[i-1]);
+        
+        return dp[n-1];
 
 */
 
