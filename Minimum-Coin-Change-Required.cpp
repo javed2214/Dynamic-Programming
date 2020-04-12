401601d416df313e9aa7a2a79314059f3852af51
Javed Ansari <46120375+javed2214@users.noreply.github.com>
2020-04-12 08:49:50 +0530

    Update Minimum-Coin-Change-Required.cpp

--- Minimum-Coin-Change-Required.cpp
+++ Minimum-Coin-Change-Required.cpp
@@ -4,18 +4,15 @@
 
 class Solution {
 public:
-    int coinChange(vector<int>& coins, int amount){
-
-    	vector <int> dp (amount + 1, amount + 1);	// Making dp[] array and filled with (amount + 1)
-    	dp[0] = 0;
-
-    	for(int i=0;i<=amount;i++){
-    		for(int j=0;j<coins.size();j++){
-    			if(coins[j] <= i)
-    				dp[i] = min(dp[i], 1 + dp[i-coins[j]]);
-    		}
-    	}
-    	if(dp[amount] == (amount + 1)) return -1;
-    	return dp[amount];
+    int coinChange(vector<int>& coins, int amount) {
+        
+        vector <int> v(amount+1, amount+1);
+        v[0] = 0;
+        
+        for(int i=1;i<amount+1;i++)
+            for(int j=0;j<coins.size();j++)
+                if(coins[j] <= i) v[i] = min(1 + v[i-coins[j]], v[i]);
+            
+        return v[amount] != (amount + 1) ? v[amount] : -1;
     }
 };
