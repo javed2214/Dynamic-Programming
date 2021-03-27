bcbeb82da2746a1dbc860b630762daff4566af33
Javed Ansari <46120375+javed2214@users.noreply.github.com>
2021-03-27 11:02:41 +0530

    Update Equal-Partition-Subset-Sum-LeetCode.cpp

--- Equal-Partition-Subset-Sum-LeetCode.cpp
+++ Equal-Partition-Subset-Sum-LeetCode.cpp
@@ -6,35 +6,30 @@
 
 class Solution {
 public:
-
-	bool subsetSum(vector <int> &v, int sum){
-
-		int n = v.size();
-
-		int dp[n+1][sum+1];
-
-		for(int i=0;i<=n;i++) dp[i][0] = true;
-		for(int i=1;i<=sum;i++) dp[0][i] = false;
-
-		for(int i=1;i<=n;i++){
-			for(int j=1;j<=sum;j++){
-				if(dp[i-1][j] == true) dp[i][j] = true;
-				else{
-					if(v[i-1] > j) dp[i][j] = false;
-					else dp[i][j] = dp[i-1][j-v[i-1]];
-				}
-			}
-		}
-		return dp[n][sum];
-	}
-
-	bool canPartition(vector<int>& nums){
-
-		int sum = 0;
-
-		for(auto it: nums) sum += it;
-
-		if(sum & 1) return false;
-		else return subsetSum(nums, sum / 2);
-	}
+    
+    bool subsetSum(vector<int> &v, int sum){
+        
+        int n = v.size();
+        bool dp[n+1][sum+1];
+        
+        for(int i = 0; i <= n; i++) dp[i][0] = true;
+        for(int j = 1; j <= sum; j++) dp[0][j] = false;
+        
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= sum; j++){
+                if(v[i-1] > j) dp[i][j] = dp[i-1][j];
+                else dp[i][j] = dp[i-1][j] or dp[i-1][j-v[i-1]];
+            }
+        }
+        if(dp[n][sum]) return true;
+        return false;
+    }
+    
+    bool canPartition(vector<int>& nums) {
+        
+        int sum = 0;
+        for(auto it: nums) sum += it;
+        if(sum % 2 != 0) return false;
+        return subsetSum(nums, sum / 2);
+    }
 };
