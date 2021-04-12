037bf6efc457eacb19ee6613402fb70e294ade4c
Javed Ansari <46120375+javed2214@users.noreply.github.com>
2021-04-12 12:14:26 +0530

    Update Longest-Bitonic-Subsequence.cpp

--- Longest-Bitonic-Subsequence.cpp
+++ Longest-Bitonic-Subsequence.cpp
@@ -2,47 +2,35 @@
 // https://www.geeksforgeeks.org/longest-bitonic-subsequence-dp-15/
 // LBS = max(LIS + LDS - 1)
 
-#include<bits/stdc++.h>
-using namespace std;
 
-int LBS(int *a, int n){
-
-	int lis[n];
-
-	for(int i=0;i<n;i++) lis[i] = 1;
-
-	for(int i=1;i<n;i++){
-		for(int j=0;j<i;j++){
-			if(a[i] > a[j] and lis[i] < lis[j] + 1)
-				lis[i] = lis[j] + 1;
-		}
+class Solution{
+	public:
+	int LongestBitonicSequence(vector<int> a){
+	    
+	    int n = a.size();
+	    
+	    int lis[n], lds[n];
+	    for(int i = 0; i < n; i++) lis[i] = 1, lds[i] = 1;
+	    
+	    for(int i = 0; i < n; i++){
+	        for(int j = 0; j < i; j++){
+	            if(a[i] > a[j])
+	                lis[i] = max(lis[i], 1 + lis[j]);
+	        }
+	    }
+	    
+	    for(int i = n-1; i >= 0; i--){
+	        for(int j = n-1; j > i; j--){
+	            if(a[i] > a[j])
+	                lds[i] = max(lds[i], 1 + lds[j]);
+	        }
+	    }
+	    
+	    int mx = 0;
+	    
+	    for(int i = 0; i < n; i++){
+	        mx = max(mx, lis[i] + lds[i] - 1);
+	    }
+	    return mx;
 	}
-
-	int lds[n];
-
-	for(int i=0;i<n;i++) lds[i] = 1;
-
-	for(int i=1;i<n;i++){
-		for(int j=0;j<i;j++){
-			if(a[i] < a[j] and lds[i] < lds[j] + 1)
-				lds[i] = lds[j] + 1;
-		}
-	}
-
-	int mx = -1;
-
-	for(int i=0;i<n;i++)
-		mx = max(mx, lis[i] + lds[i] - 1);
-
-	return mx;
-}
-
-int main(){
-
-	int a[] = {80,60,30,40,20,10};
-	int n = sizeof(a) / sizeof(int);
-
-	cout<<LBS(a,n);
-
-	return 0;
-}
+};
